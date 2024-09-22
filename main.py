c9ba4323402dabf17e504c911319759e74438248
hasederhi <hasederhi.12@gmail.com>
2024-09-22 19:19:48 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from tkinter import font
+import time
+import random
+from datetime import datetime
+import winsound
+
+CEND      = '\33[0m'
+CBOLD     = '\33[1m'
+CITALIC   = '\33[3m'
+CURL      = '\33[4m'
+CBLINK    = '\33[5m'
+CBLINK2   = '\33[6m'
+CSELECTED = '\33[7m'
+
+CBLACK  = '\33[30m'
+CRED    = '\33[31m'
+CGREEN  = '\33[32m'
+CYELLOW = '\33[33m'
+CBLUE   = '\33[34m'
+CVIOLET = '\33[35m'
+CBEIGE  = '\33[36m'
+CWHITE  = '\33[37m'
+
+CBLACKBG  = '\33[40m'
+CREDBG    = '\33[41m'
+CGREENBG  = '\33[42m'
+CYELLOWBG = '\33[43m'
+CBLUEBG   = '\33[44m'
+CVIOLETBG = '\33[45m'
+CBEIGEBG  = '\33[46m'
+CWHITEBG  = '\33[47m'
+
+CGREY    = '\33[90m'
+CRED2    = '\33[91m'
+CGREEN2  = '\33[92m'
+CYELLOW2 = '\33[93m'
+CBLUE2   = '\33[94m'
+CVIOLET2 = '\33[95m'
+CBEIGE2  = '\33[96m'
+CWHITE2  = '\33[97m'
+
+CGREYBG    = '\33[100m'
+CREDBG2    = '\33[101m'
+CGREENBG2  = '\33[102m'
+CYELLOWBG2 = '\33[103m'
+CBLUEBG2   = '\33[104m'
+CVIOLETBG2 = '\33[105m'
+CBEIGEBG2  = '\33[106m'
+CWHITEBG2  = '\33[107m'
+
+displayWindow = Tk()
+displayWindow.title("Passcode Input")
+displayWindow.geometry("800x600")
+displayWindow.configure(bg="#b3b3b3")
+displayWindow.resizable(True,True)
+
+frame1 = Frame(displayWindow, width=800, height=60, bg='red')
+frame1.place(x=0, y=0, relx=0.00, rely=0.00)
+
+display_main = Label(text="NO DATA", font=("TkDefaultFont", 40),height=1, width=27)
+display_main.grid(row=0, column=0)
+
+displayWindow.mainloop()
